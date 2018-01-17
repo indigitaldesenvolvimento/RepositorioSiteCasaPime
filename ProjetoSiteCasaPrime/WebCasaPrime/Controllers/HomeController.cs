fc02eef4c766fdb2091c91892bb5c16d8f490280
igorlucas <igorlucas.silva@bol.com.br>
2018-01-17 02:04:06 -0300

    subindo o projeto inicial

---
+++ ProjetoSiteCasaPrime/WebCasaPrime/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebSiteCasaPrime.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult Servico()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
